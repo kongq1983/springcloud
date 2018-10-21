9bf2c21246970be99ed4212e8ca5f5e6b060e312
kq <kongq1983@yeah.net>
2018-10-21 23:55:52 +0800

    first commit

---
+++ userservice/src/main/java/com/kq/springcloud/userservice/UserServiceApplication.java
@@ -0,0 +1,16 @@
+package com.kq.springcloud.userservice;
+
+/**
+ * Created by qikong on 18/10/21.
+ */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@EnableDiscoveryClient
+@SpringBootApplication
+public class UserServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+}
---
+++ userservice/src/main/java/com/kq/springcloud/userservice/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.kq.springcloud.userservice.controller;
+
+/**
+ * Created by qikong on 18/10/21.
+ */
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class UserController {
+
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
+
+    @GetMapping("/user-instance")
+    public List<ServiceInstance> showInfo(){
+        return this.discoveryClient.getInstances("userservice");
+    }
+
+
+    @GetMapping("/{id}")
+    public String findById(@PathVariable Long id) {
+        return String.valueOf(id);
+    }
+}
