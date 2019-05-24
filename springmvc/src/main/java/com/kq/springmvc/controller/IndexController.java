60742b725162443ba934cb497e4f2873efe31ea5
king <king@qq.com>
2019-05-24 17:29:19 +0800

    添加getIp

--- springmvc/src/main/java/com/kq/springmvc/controller/IndexController.java
+++ springmvc/src/main/java/com/kq/springmvc/controller/IndexController.java
@@ -4,6 +4,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.InetAddress;
+
 /**
  * IndexController
  *
@@ -18,5 +20,30 @@ public class IndexController {
     public String register() {
         return "indextest";
     }
+
+    @ResponseBody
+    @RequestMapping( value = "/getIp")
+    public String getLocalhostIp(){
+        InetAddress ia=null;
+        try {
+            ia=ia.getLocalHost();
+
+            String localname=ia.getHostName();
+            String localip=ia.getHostAddress();
+            System.out.println("本机名称是："+ localname);
+            System.out.println("本机的ip是 ："+localip);
+
+            return "localIp="+localip;
+
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+
+            return "error="+e.getMessage();
+        }
+    }
+
+
+
 
 }
